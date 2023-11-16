ffb802c8ccc36195b0faf5042dbdb44b23922219
Paul Philip Mitchell <89913982+pauphi@users.noreply.github.com>
2023-11-16 12:59:15 +0100

    Create ChocoApplications.ps1

---
+++ src/Applications/ChocoApplications.ps1
@@ -0,0 +1,7 @@
+choco install -y "jetbrains-rider";
+choco install -y "notion";
+choco install -y "th-ch-youtube-music";
+choco install -y "slack";
+choco install -y "bitwarden";
+choco install -y "logioptionsplus";
+choco install -y "powertoys";
